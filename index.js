ba5d62c588e2569f79758e6c7e78c16c459d5c53
amme987 <emmagination97@gmail.com>
2024-09-12 12:04:34 -0400

    Find available moves for current coordinate

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-function knightMoves() {}
+// let board = [];
+// for (let i = 0; i < 8; i++) {
+//   board.push([]);
+//   for (let j = 0; j < 8; j++) {
+//     board[i].push([i, j]);
+//   }
+// }
 
-let board = [];
-for (let i = 0; i < 8; i++) {
-  board.push([]);
-  for (let j = 0; j < 8; j++) {
-    board[i].push([i, j]);
-  }
+// console.log(board);
+
+// knightMoves([0,0],[3,3]) == [[0,0],[2,1],[3,3]] or knightMoves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
+// knightMoves([3,3],[0,0]) == [[3,3],[2,1],[0,0]] or knightMoves([3,3],[0,0]) == [[3,3],[1,2],[0,0]]
+// knightMoves([0,0],[7,7]) == [[0,0],[2,1],[4,2],[6,3],[4,4],[6,5],[7,7]] or knightMoves([0,0],[7,7]) == [[0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]]
+
+// Directions knight can move
+let offsets = [
+  [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
+  [-1, -2],
+  [-2, -1],
+  [-2, 1],
+  [-1, 2],
+];
+
+function knightMoves(start, end) {
+  // let visited = new Array(64).fill(false);
+  let visited = [start];
+  let queue = [start];
+
+  // while (queue) {
+  let curr = queue.shift();
+
+  // If current coordinate + offset is on the board, push the resulting corrdinate into queue and visited array
+  offsets.forEach(([x, y]) => {
+    if (
+      curr[0] + x >= 0 &&
+      curr[0] + x < 8 &&
+      curr[1] + y >= 0 &&
+      curr[1] + y < 8
+    ) {
+      visited.push([curr[0] + x, curr[1] + y]);
+      queue.push([curr[0] + x, curr[1] + y]);
+    }
+  });
+  return queue;
+  // }
 }
 
-console.log(board);
-
-// let board = Array.from({ length: 8 }, () => new Array(0, 1, 2, 3, 4, 5, 6, 7));
-// // board.forEach(value => (value = Array.from({ length: 8 }, () => '')));
-// // console.log(board);
-// console.log(board[1][2]);
+console.log(knightMoves([0, 0], [3, 3]));
